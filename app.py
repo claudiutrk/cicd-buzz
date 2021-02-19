0c42114479bb905103b5ef73ea7b4bccd20db390
claudiutrk <turcu.claudiu.wow@gmail.com>
2021-02-19 22:14:25 +0200

    :hammer:

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 from buzz import generator
 
 app = Flask(__name__)
@@ -9,7 +9,22 @@ def generate_buzz():
     page = '<html><body><h1>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
+    
     return page
+    
+@app.route("/home/")    
+def home():
+    return render_template("index.html")
+    
+@app.route("/<niem>")
+def user(niem):
+    return render_template("index.html", content=niem)
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+    
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
